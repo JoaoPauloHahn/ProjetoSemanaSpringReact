0b3869a78bdbf3dfbf8bd2a86b0b51536e107ba7
JoaoPauloHahn <joaohahn22@gmail.com>
2022-01-12 19:34:32 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/dsmovie/ProjetoSemanaReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemanaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemanaReactApplication.class, args);
+	}
+
+}
